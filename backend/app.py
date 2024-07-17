60c5919e09011fe11e21e608b21045389967292b
Julia Showl <juliashowl@gwu.edu>
2024-07-16 20:01:58 -0400

    Complete API

--- backend/app.py
+++ backend/app.py
@@ -1,77 +1,56 @@
-from flask import Flask, render_template, session, jsonify, json
-import os
+from flask import Flask, request, json, jsonify
+from datetime import datetime
 
-app = Flask('__name__')
-app.secret_key = os.environ['SECRET']
+app = Flask(__name__)
 
-file = open(os.path.dirname(os.path.realpath(__file__)) + "/data/jr_data.json")
-data = json.load(file)
+with open("./data/jr_data.json", "r") as file:
+    data = json.load(file)
 
-## Homepage
-@app.route('/')
-def index():
-    return render_template('index.html', home=True)
+valid_statuses = ["In Draft", "In Review", "Pending Approval", "Complete"]
+json_statuses = {"In Draft": "inDraft", "In Review": "inReview", "Pending Approval": "pendingApproval", "Complete": "complete"}
 
 ## Returns user information
 @app.route('/user', methods=['GET'])
-def user():
-    return render_template('user.html', message=data['user'])
+def get_user():
+    user = data['user']
+    return jsonify({"user": user})
 
 ## Returns overall document statistics
 @app.route('/stats', methods=['GET'])
-def stats():
-    return render_template('user.html', message=data['stats'])
+def get_stats():
+    stats = data['stats']
+    return jsonify({"stats": stats})
 
 ##  Returns the list of documents
 @app.route('/documents', methods=['GET'])
-def doc():
-    return render_template('user.html', message=data['documents'])
+def get_docs():
+    docs = data['documents']
+    return jsonify({"docs": docs})
 
 ##  Updates a document's status
 @app.route('/documents/<int:id>', methods=['PUT'])
-def status(id:int):
-    return
-
-## Error handlers
-@app.errorhandler(404)
-def not_found(e):
-    session.clear()
-    return render_template('incorrect.html', message="Sorry that page can't be found"), 404
-
-@app.errorhandler(500)
-def server_error(e):
-    session.clear()
-    return render_template('incorrect.html', message="Sorry we ran into a server error"), 500
-
-@app.errorhandler(401)
-def auth_error(e):
-    session.clear()
-    return render_template('incorrect.html', message="Sorry you're not authorized to access that"), 401
-
-@app.errorhandler(400)
-def bad_req(e):
-    session.clear()
-    return render_template('incorrect.html', message="Sorry you sent a bad request"), 400
-
-@app.errorhandler(403)
-def forbiden(e):
-    session.clear()
-    return render_template('incorrect.html', message="Sorry you can't do that"), 403
-
-@app.errorhandler(501)
-def unsupported(e):
-    session.clear()
-    return render_template('incorrect.html', message="Sorry your browser doesn't support that"), 501
-
-@app.errorhandler(502)
-def traffic(e):
-    session.clear()
-    return render_template('incorrect.html', message="Sorry we're expecting heavy traffic at the moment"), 502
-
-@app.errorhandler(503)
-def unavailable(e):
-    session.clear()
-    return render_template('incorrect.html', message="Sorry the service you're trying to reach is unavailable"), 503
+def update_status(id):
+    new_status = request.json.get("status")
+    if not new_status or new_status not in valid_statuses:
+        return jsonify({"message": "Unable to process request"}, 400)
+    
+    for d in data['documents']:
+        if d.get('id') == id:
+            current_status = json_statuses[d.get('status')]
+            d['status'] = new_status
+            d['lastEdited'] = datetime.today().strftime("%Y-%m-%d")
+            with open("./data/jr_data.json", "w", encoding='utf-8') as f:
+                json.dump(data, f, indent=2)
+            
+            stats = data['stats']
+            new_status = json_statuses[new_status]
+            stats[current_status] -= 1
+            stats[new_status] += 1
+            with open("./data/jr_data.json", "w", encoding='utf-8') as f:
+                json.dump(data, f, indent=2)
+            return jsonify({"message": "Update complete"}, 201)
+        
+    return jsonify({"message": "Unable to find document"}, 404)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
