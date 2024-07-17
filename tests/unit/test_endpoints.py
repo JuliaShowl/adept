a998d7ce2c4e563a02b97cf5aaa22b138f9e1236
Julia Showl <juliashowl@gwu.edu>
2024-07-17 10:21:32 -0400

    Modify testing

--- tests/unit/test_endpoints.py
+++ tests/unit/test_endpoints.py
@@ -1,9 +1,14 @@
-from conftest import client
+import sys, os
+
+parent_directory = os.path.abspath('../../')
+sys.path.append(parent_directory)
+
+from backend.app import app 
 
 def test_status_codes():
-    user = client.get('/user')
-    stats = client.get('/stats')
-    documents = client.get('/documents')
+    user = app.test_client().get('/user')
+    stats = app.test_client().get('/stats')
+    documents = app.test_client().get('/documents')
     assert user.status_code == 200
     assert stats.status_code == 200
     assert documents.status_code == 200
