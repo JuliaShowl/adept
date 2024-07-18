79e1d35bfecc857f9493c29fca1d4296c016a646
Julia Showl <juliashowl@gwu.edu>
2024-07-18 19:00:17 -0400

    Test update API endpoint

--- tests/unit/test_endpoints.py
+++ tests/unit/test_endpoints.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, json
 
 parent_directory = os.path.abspath('../../')
 sys.path.append(parent_directory)
@@ -12,3 +12,12 @@ def test_status_codes():
     assert user.status_code == 200
     assert stats.status_code == 200
     assert documents.status_code == 200
+
+
+def test_update():
+    invalid_status = app.test_client().put('/documents/1', data={'status': 'test'}, headers={'Content-Type': 'application/json', 'charset': 'utf-8'})
+    invalid_doc = app.test_client().put('/documents/7', data={'status': 'In Review'}, headers={'Content-Type': 'application/json', 'charset': 'utf-8'})
+    invalid_payload = app.test_client().put('/documents/7', headers={'Content-Type': 'application/json', 'charset': 'utf-8'})
+    assert invalid_status.status_code == 400
+    assert invalid_doc.status_code == 400
+    assert invalid_payload.status_code == 400
